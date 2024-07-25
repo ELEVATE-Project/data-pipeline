3bbbd90fe571293959ed0376df06aa12372ec070
vivek <user@PWA177.local>
2024-07-25 16:40:20 +0530

    added PostgresUtil file

--- project-jobs/src/main/scala/org/shikshalokam/job/project/functions/ProjectStreamFunction.scala
+++ project-jobs/src/main/scala/org/shikshalokam/job/project/functions/ProjectStreamFunction.scala
@@ -5,13 +5,13 @@ import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.ProcessFunction
 import org.shikshalokam.job.project.domain.Event
 import org.shikshalokam.job.project.task.ProjectStreamConfig
+import org.shikshalokam.job.util.PostgresUtil
 import org.shikshalokam.job.{BaseProcessFunction, Metrics}
 import org.slf4j.LoggerFactory
 
 import scala.collection.immutable._
 
-
-class ProjectStreamFunction(config: ProjectStreamConfig)(implicit val mapTypeInfo: TypeInformation[Event]/**, @transient var mongoUtil: MongoUtil = null*/)
+class ProjectStreamFunction(config: ProjectStreamConfig)(implicit val mapTypeInfo: TypeInformation[Event], @transient var postgresUtil: PostgresUtil = null)
   extends BaseProcessFunction[Event, Event](config) {
 
   private[this] val logger = LoggerFactory.getLogger(classOf[ProjectStreamFunction])
@@ -22,16 +22,20 @@ class ProjectStreamFunction(config: ProjectStreamConfig)(implicit val mapTypeInf
 
   override def open(parameters: Configuration): Unit = {
     super.open(parameters)
-//    mongoUtil = new MongoUtil(config.dbHost, config.dbPort, config.dataBase)
+    val pgHost: String = config.pgHost
+    val pgPort: String = config.pgPort
+    val pgUsername: String = config.pgUsername
+    val pgPassword: String = config.pgPassword
+    val pgDataBase: String = config.pgDataBase
+    val connectionUrl: String = s"jdbc:postgresql://$pgHost:$pgPort/$pgDataBase"
+    postgresUtil = new PostgresUtil(connectionUrl, pgUsername, pgPassword)
   }
 
   override def close(): Unit = {
-//    mongoUtil.close()
     super.close()
   }
 
   override def processElement(event: Event, context: ProcessFunction[Event, Event]#Context, metrics: Metrics): Unit = {
-
 
 
     val (projectEvidences, projectEvidencesCount) = extractEvidenceData(event.projectAttachments)
@@ -45,26 +49,109 @@ class ProjectStreamFunction(config: ProjectStreamConfig)(implicit val mapTypeInf
     //val orgIdsString = organisationsData.map(_("orgId")).mkString(", ")
     //println(orgIdsString)
 
-    val locationsDada = extractLocationsData(event.locations)
-    println(locationsDada)
+    val locationsData = extractLocationsData(event.locations)
+    println(locationsData)
+
+    postgresUtil.createTable(config.createSolutionsTable, config.solutionsTable)
+    postgresUtil.createTable(config.createProjectTable, config.projectsTable)
+
+    val solutionId = event.solutionId
+    val solutionExternalId = event.solutionExternalId
+    val solutionName = event.solutionName
+    val solutionDescription = event.solutionDescription
+    val projectDuration = event.projectDuration
+    val hasAcceptedTAndC = event.hasAcceptedTAndC
+    val projectIsDeleted = event.projectIsDeleted
+    val projectCreatedType = event.projectCreatedType
+    val programId = event.programId
+    val programName = event.programName
+    val programExternalId = event.programExternalId
+    val programDescription = event.programDescription
+    val privateProgram = event.privateProgram
 
 
+    val insertSolutionQuery =
+      s"""INSERT INTO Solutions (solutionId, externalId, name, description, duration, hasAcceptedTAndC, isDeleted, createdType, programId, programName, programExternalId, programDescription, privateProgram)
+         |VALUES ('$solutionId', '$solutionExternalId', '$solutionName', '$solutionDescription', '$projectDuration', '$hasAcceptedTAndC', $projectIsDeleted, '$projectCreatedType', '$programId', '$programName', '$programExternalId', '$programDescription', $privateProgram);
+         |""".stripMargin
+
+//    postgresUtil.insertData(insertSolutionQuery)
+
+    /**
+     * Extracting Project data
+     */
+    val projectId = event.projectId
+    val createdBy = event.createdBy
+    val completedDate = event.completedDate
+    val createdDate = event.createdAt
+    val (evidence, evidenceCount) = extractEvidenceData(event.projectAttachments)
+    val lastSync = event.projectLastSync
+    val remarks = event.projectRemarks
+    val updatedDate = event.projectUpdatedDate
+    val projectStatus = event.projectStatus
+    val organisationId = organisationsData.map(_("orgId")).mkString(", ")
+    val stateCode = extractLocationDetail(locationsData, "stateCode")
+    val stateExternalId = extractLocationDetail(locationsData, "stateExternalId")
+    val stateName = extractLocationDetail(locationsData, "stateName")
+    val districtCode = extractLocationDetail(locationsData, "districtCode")
+    val districtExternalId = extractLocationDetail(locationsData, "districtExternalId")
+    val districtName = extractLocationDetail(locationsData, "districtName")
+    val blockCode = extractLocationDetail(locationsData, "blockCode")
+    val blockExternalId = extractLocationDetail(locationsData, "blockExternalId")
+    val blockName = extractLocationDetail(locationsData, "blockName")
+    val clusterCode = extractLocationDetail(locationsData, "clusterCode")
+    val clusterExternalId = extractLocationDetail(locationsData, "clusterExternalId")
+    val clusterName = extractLocationDetail(locationsData, "clusterName")
+    val schoolCode = extractLocationDetail(locationsData, "schoolCode")
+    val schoolExternalId = extractLocationDetail(locationsData, "schoolExternalId")
+    val schoolName = extractLocationDetail(locationsData, "schoolName")
+    val boardName = event.boardName
+
+    val updateProjectQuery =
+      s"""UPDATE Projects
+         |SET
+         |    createdBy = '$createdBy',
+         |    solutionId = '$solutionId',
+         |    programId = '$programId',
+         |    completedDate = '$completedDate',
+         |    createdDate = '$createdDate',
+         |    evidence = '$evidence',
+         |    evidenceCount = '$evidenceCount',
+         |    lastSync = '$lastSync',
+         |    remarks = '$remarks',
+         |    updatedDate = '$updatedDate',
+         |    projectStatus = '$projectStatus',
+         |    organisationId = '$organisationId',
+         |    stateCode = '$stateCode',
+         |    stateExternalId = '$stateExternalId',
+         |    stateName = '$stateName',
+         |    districtCode = '$districtCode',
+         |    districtExternalId = '$districtExternalId',
+         |    districtName = '$districtName',
+         |    blockCode = '$blockCode',
+         |    blockExternalId = '$blockExternalId',
+         |    blockName = '$blockName',
+         |    clusterCode = '$clusterCode',
+         |    clusterExternalId = '$clusterExternalId',
+         |    clusterName = '$clusterName',
+         |    schoolCode = '$schoolCode',
+         |    schoolExternalId = '$schoolExternalId',
+         |    schoolName = '$schoolName',
+         |    boardName = '$boardName'
+         |WHERE
+         |    projectId = '$projectId';
+         |""".stripMargin
 
 
-
-
-
+    try {
+      postgresUtil.insertData(updateProjectQuery) // Assuming insertData can also execute UPDATE statements
+      println("Record updated successfully.")
+    } catch {
+      case e: Exception =>
+        println("Error occurred while updating the record: " + e.getMessage)
     }
 
-
-
-
-
-
-
-
-
-
+  }
 
 
   def extractEvidenceData(Attachments: List[Map[String, Any]]): (String, Int) = {
@@ -78,11 +165,11 @@ class ProjectStreamFunction(config: ProjectStreamConfig)(implicit val mapTypeInf
     (evidenceList.mkString(", "), evidenceList.length)
   }
 
-//  def extractOrganisationsData(Organisations: List[Map[String, Any]]): (String, String) = {
-//    val orgId = Organisations.map(organisation => organisation.get("organisationId").map(id => if (id.toString.trim.isEmpty) "Null" else id.toString).getOrElse("Null"))
-//    val orgName = Organisations.map(organisation => organisation.get("orgName").map(name => if (name.toString.trim.isEmpty) "Null" else name.toString).getOrElse("Null"))
-//    (orgId.mkString(", "), orgName.mkString(", "))
-//  }
+  //  def extractOrganisationsData(Organisations: List[Map[String, Any]]): (String, String) = {
+  //    val orgId = Organisations.map(organisation => organisation.get("organisationId").map(id => if (id.toString.trim.isEmpty) "Null" else id.toString).getOrElse("Null"))
+  //    val orgName = Organisations.map(organisation => organisation.get("orgName").map(name => if (name.toString.trim.isEmpty) "Null" else name.toString).getOrElse("Null"))
+  //    (orgId.mkString(", "), orgName.mkString(", "))
+  //  }
 
   def extractOrganisationsData(organisations: List[Map[String, Any]]): List[Map[String, String]] = {
     organisations.map { organisation =>
@@ -105,6 +192,12 @@ class ProjectStreamFunction(config: ProjectStreamConfig)(implicit val mapTypeInf
         ))
       }
     }
+  }
+
+  def extractLocationDetail(locationsData: List[Map[String, String]], key: String): String = {
+    locationsData.collectFirst {
+      case location if location.contains(key) => location(key)
+    }.getOrElse("Null")
   }
 
 
--- project-jobs/src/main/scala/org/shikshalokam/job/project/task/ProjectStreamConfig.scala
+++ project-jobs/src/main/scala/org/shikshalokam/job/project/task/ProjectStreamConfig.scala
@@ -23,10 +23,72 @@ class ProjectStreamConfig(override val config: Config) extends BaseJobConfig(con
   // Functions
   val projectsStreamFunction: String = "ProjectStreamFunction"
 
-  // User delete job metrics
+  // Project submissions job metrics
   val projectsCleanupHit = "project-cleanup-hit"
   val skipCount = "skipped-message-count"
   val successCount = "success-message-count"
-  val totalEventsCount = "total-transfer-events-count"
+  val totalEventsCount = "total-project-events-count"
+
+  // PostgreSQL connection config
+  val pgHost: String = config.getString("postgres.host")
+  val pgPort: String = config.getString("postgres.port")
+  val pgUsername: String = config.getString("postgres.username")
+  val pgPassword: String = config.getString("postgres.password")
+  val pgDataBase: String = config.getString("postgres.database")
+
+  // PostgreSQL query config
+  val solutionsTable = "Solutions"
+  val projectsTable = "Projects"
+  val tasksTable = "Tasks"
+  val organisationsTable = "Organisations"
+
+  val createSolutionsTable = """CREATE TABLE IF NOT EXISTS Solutions (
+                         |    solutionId TEXT PRIMARY KEY,
+                         |    externalId TEXT,
+                         |    name TEXT,
+                         |    description TEXT,
+                         |    duration TEXT,
+                         |    hasAcceptedTAndC TEXT,
+                         |    isDeleted BOOLEAN,
+                         |    createdType TEXT,
+                         |    programId TEXT,
+                         |    programName TEXT,
+                         |    programExternalId TEXT,
+                         |    programDescription TEXT,
+                         |    privateProgram BOOLEAN
+                         |);
+                         |""".stripMargin
+
+  val createProjectTable = """CREATE TABLE IF NOT EXISTS Projects (
+                             |    projectId TEXT PRIMARY KEY,
+                             |    createdBy TEXT,
+                             |    solutionId TEXT,
+                             |    programId TEXT,
+                             |    completedDate TEXT,
+                             |    createdDate TEXT,
+                             |    evidence TEXT,
+                             |    evidenceCount TEXT,
+                             |    lastSync TEXT,
+                             |    remarks TEXT,
+                             |    updatedDate DATE,
+                             |    projectStatus TEXT,
+                             |    organisationId TEXT,
+                             |    stateCode TEXT,
+                             |    stateExternalId TEXT,
+                             |    stateName TEXT,
+                             |    districtCode TEXT,
+                             |    districtExternalId TEXT,
+                             |    districtName TEXT,
+                             |    blockCode TEXT,
+                             |    blockExternalId TEXT,
+                             |    blockName TEXT,
+                             |    clusterCode TEXT,
+                             |    clusterExternalId TEXT,
+                             |    clusterName TEXT,
+                             |    schoolCode TEXT,
+                             |    schoolExternalId TEXT,
+                             |    schoolName TEXT,
+                             |    boardName TEXT
+                             |);""".stripMargin
 
 }
