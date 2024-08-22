7907587b5e33f2b407c3d105d0a663ea9f11cd8c
vivek <user@PWA177.lan>
2024-08-22 14:47:56 +0530

    changes in org and location data

--- project-jobs/src/main/scala/org/shikshalokam/job/project/domain/Event.scala
+++ project-jobs/src/main/scala/org/shikshalokam/job/project/domain/Event.scala
@@ -4,7 +4,7 @@ import org.shikshalokam.job.domain.reader.JobRequest
 
 class Event(eventMap: java.util.Map[String, Any], partition: Int, offset: Long) extends JobRequest(eventMap, partition, offset) {
 
-  println("inside event ")
+ def _id: String = readOrDefault[String]("_id", "")
 
   def solutionId: String = readOrDefault[String]("solutionInformation._id", "")
 
@@ -36,11 +36,9 @@ class Event(eventMap: java.util.Map[String, Any], partition: Int, offset: Long) 
 
   def createdBy: String = readOrDefault[String]("createdBy", "")
 
-  def completedDate: String = if (readOrDefault[String]("status", "") == "submitted") readOrDefault[String]("updatedAt", "None") else "None"
-
   def createdAt: String = readOrDefault[String]("createdAt", "")
 
-  def projectAttachments: List[Map[String, Any]] = readOrDefault[List[Map[String, Any]]]("attachments", null)
+  def completedDate: String = if (readOrDefault[String]("status", "") == "submitted") readOrDefault[String]("updatedAt", "None") else "None"
 
   def projectLastSync: String = readOrDefault[String]("syncedAt", "")
 
@@ -50,54 +48,39 @@ class Event(eventMap: java.util.Map[String, Any], partition: Int, offset: Long) 
 
   def projectStatus: String = readOrDefault[String]("status", "")
 
-  def organisations: List[Map[String, Any]] = readOrDefault[List[Map[String, Any]]]("userProfile.organisations", null)
+  def organisationId: String = readOrDefault[Int]("userProfile.organization.id", 0).toString
 
-  def locations: List[Map[String, Any]] = readOrDefault[List[Map[String, Any]]]("userProfile.userLocations", null)
+  def organisationName: String = readOrDefault[String]("userProfile.organization.name", "")
 
-  def boardName: String = {
-    val boardList: List[String] = readOrDefault[List[String]]("userProfile.framework.board", List())
-    boardList.mkString(", ")
-  }
+  def organisationCode: String = readOrDefault[String]("userProfile.organization.code", "")
+
+  def stateId: String = readOrDefault[String]("userProfile.state.value", "")
+
+  def stateName: String = readOrDefault[String]("userProfile.state.label", "")
+
+  def districtId: String = readOrDefault[String]("userProfile.district.value", "")
+
+  def districtName: String = readOrDefault[String]("userProfile.district.label", "")
+
+  def blockId: String = readOrDefault[String]("userProfile.block.value", "")
+
+  def blockName: String = readOrDefault[String]("userProfile.block.label", "")
+
+  def clusterId: String = readOrDefault[String]("userProfile.cluster.value", "")
+
+  def clusterName: String = readOrDefault[String]("userProfile.cluster.label", "")
+
+  def schoolId: String = readOrDefault[String]("userProfile.school.value", "")
+
+  def schoolName: String = readOrDefault[String]("userProfile.school.label", "")
+
+  def userRoles: List[Map[String, Any]] = readOrDefault[List[Map[String, Any]]]("userProfile.user_roles", null)
+
+  def projectAttachments: List[Map[String, Any]] = readOrDefault[List[Map[String, Any]]]("attachments", null)
+
   def tasks: List[Map[String, Any]] = readOrDefault[List[Map[String, Any]]]("tasks", null)
 
   val taskCount = tasks.size
-
-//  val taskCount = Option(tasks).map(_.size).getOrElse(0)
-  println(taskCount)
-  println("before tasks count")
-  Thread.sleep(5000)
-
-  println("\n")
-  println("solutionId = " + solutionId)
-  println("solutionExternalId = " + solutionExternalId)
-  println("solutionName = " + solutionName)
-  println("solutionDescription = " + solutionDescription)
-
-  println("\n")
-  println("projectDuration = " + projectDuration)
-  println("hasAcceptedTAndC = " + hasAcceptedTAndC)
-  println("projectIsDeleted = " + projectIsDeleted)
-  println("projectCreatedType = " + projectCreatedType)
-  println("privateProgram = " + privateProgram)
-
-  println("\n")
-  println("programId = " + programId)
-  println("programExternalId = " + programExternalId)
-  println("programName = " + programName)
-  println("programDescription = " + programDescription)
-
-  println("\n")
-  println("projectId = " + projectId)
-  println("createdBy = " + createdBy)
-  println("completedDate = " + completedDate)
-  println("createdAt = " + createdAt)
-  println("projectLastSync = " + projectLastSync)
-  println("projectRemarks = " + projectRemarks)
-  println("projectUpdatedDate = " + projectUpdatedDate)
-  println("projectStatus = " + projectStatus)
-  println("boardName = " +boardName)
-
-  println(locations)
 
 }
 
