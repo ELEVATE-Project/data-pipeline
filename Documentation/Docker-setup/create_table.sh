eb22746be4dbed36d81c6cc1c46e31813937bd1f
prashanth <prashanth@shikshalokam.org>
2025-02-10 14:59:40 +0530

    added the docker-setup files

---
+++ Documentation/Docker-setup/create_table.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+set -a
+source .env
+set +a
+
+# Set dynamic table names
+METADATA_TABLE="${PG_ENV}_dashboard_metadata"
+SOLUTIONS_TABLE="${PG_ENV}_solutions"
+PROJECTS_TABLE="${PG_ENV}_projects"
+TASKS_TABLE="${PG_ENV}_tasks"
+
+# Check if required variables are set
+if [[ -z "$PG_HOST" || -z "$POSTGRES_PORT" || -z "$PG_DBNAME" || -z "$POSTGRES_USER" || -z "$POSTGRES_PASSWORD" ]]; then
+    echo "Error: One or more required PostgreSQL environment variables are missing."
+    exit 1
+fi
+
+# SQL script for table creation
+SQL_COMMANDS=$(cat <<EOF
+CREATE TABLE IF NOT EXISTS public."$METADATA_TABLE"
+(
+    id SERIAL PRIMARY KEY,
+    entity_type TEXT NOT NULL,
+    entity_name TEXT NOT NULL,
+    entity_id TEXT NOT NULL UNIQUE,
+    collection_id TEXT,
+    dashboard_id TEXT,
+    question_ids TEXT,
+    status TEXT,
+    error_message TEXT
+);
+
+CREATE TABLE IF NOT EXISTS public."$SOLUTIONS_TABLE"
+(
+    solution_id TEXT PRIMARY KEY,
+    external_id TEXT,
+    name TEXT,
+    description TEXT,
+    duration TEXT,
+    categories TEXT,
+    program_id TEXT,
+    program_name TEXT,
+    program_external_id TEXT,
+    program_description TEXT,
+    private_program BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS public."$PROJECTS_TABLE"
+(
+    project_id TEXT PRIMARY KEY,
+    solution_id TEXT REFERENCES public."$SOLUTIONS_TABLE"(solution_id),
+    created_by TEXT,
+    created_date TEXT,
+    completed_date TEXT,
+    last_sync TEXT,
+    updated_date TEXT,
+    status TEXT,
+    remarks TEXT,
+    evidence TEXT,
+    evidence_count TEXT,
+    program_id TEXT,
+    task_count TEXT,
+    user_role_ids TEXT,
+    user_roles TEXT,
+    org_id TEXT,
+    org_name TEXT,
+    org_code TEXT,
+    state_id TEXT,
+    state_name TEXT,
+    district_id TEXT,
+    district_name TEXT,
+    block_id TEXT,
+    block_name TEXT,
+    cluster_id TEXT,
+    cluster_name TEXT,
+    school_id TEXT,
+    school_name TEXT,
+    certificate_template_id TEXT,
+    certificate_template_url TEXT,
+    certificate_issued_on TEXT,
+    certificate_status TEXT,
+    certificate_pdf_path TEXT
+);
+
+CREATE TABLE IF NOT EXISTS public."$TASKS_TABLE"
+(
+    task_id TEXT PRIMARY KEY,
+    project_id TEXT REFERENCES public."$PROJECTS_TABLE"(project_id),
+    name TEXT,
+    assigned_to TEXT,
+    start_date TEXT,
+    end_date TEXT,
+    synced_at TEXT,
+    is_deleted TEXT,
+    is_deletable TEXT,
+    remarks TEXT,
+    status TEXT,
+    evidence TEXT,
+    evidence_count TEXT
+);
+EOF
+)
+
+# Execute SQL commands
+PGPASSWORD="$PG_PASSWORD" psql -h "$PG_HOST" -p "$PG_PORT" -d "$PG_DBNAME" -U "$PG_USER" -c "$SQL_COMMANDS"
+
+if [ $? -eq 0 ]; then
+    echo "Tables created successfully."
+else
+    echo "Error: Failed to create tables."
+fi
---
+++ Documentation/Docker-setup/deploy_flink_job.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+set -a
+source .env
+set +a
+
+# Set environment variables
+KAFKA_CONTAINER="${KAFKA_CONTAINER_NAME}"
+ELEVATE_CONTAINER="${ELEVATE_DATA_CONTAINER_NAME}"
+
+# Read Kafka topic names from environment variables or use defaults
+TOPIC1="${PROJECT_TOPIC}"
+TOPIC2="${METABASE_TOPIC}"
+
+# Load report_config data in postgres
+echo "Loading report_config data in Postgres..."
+sudo docker exec -it elevate-data chmod +x /app/metabase-jobs/config-data-loader/data-loader.sh
+sudo docker exec -it elevate-data /app/metabase-jobs/config-data-loader/data-loader.sh
+echo "report_config data loaded successfully."
+
+# 1. Create Kafka topics in the Docker container
+echo "Creating Kafka topics..."
+sudo docker exec -it $KAFKA_CONTAINER /usr/bin/kafka-topics --create --bootstrap-server kafka:9092 --replication-factor 1 --partitions 1 --topic $TOPIC1
+
+sudo docker exec -it $KAFKA_CONTAINER /usr/bin/kafka-topics --create --bootstrap-server kafka:9092 --replication-factor 1 --partitions 1 --topic $TOPIC2
+
+echo "Kafka topics created successfully."
+
+# 2. Build the Maven JAR inside the Elevate Data container
+echo "Building Maven project inside $ELEVATE_CONTAINER..."
+sudo docker exec -it $ELEVATE_CONTAINER mvn clean install -DskipTests
+
+if [ $? -ne 0 ]; then
+    echo "Maven build failed. Exiting..."
+    exit 1
+fi
+
+echo "Maven build completed successfully."
+
+# 3. Download the JAR file from the Elevate Data container
+echo "Copying JAR files from $ELEVATE_CONTAINER to local machine..."
+sudo docker cp elevate-data:/app/project-jobs/project-stream-processor/target/project-stream-processor-1.0.0.jar ./
+sudo docker cp elevate-data:/app/metabase-jobs/dashboard-creator/target/dashboard-creator-1.0.0.jar ./
+echo "JAR files copied successfully."
+
+# 4. Upload and run JARs in Flink
+upload_and_run_jar() {
+    local jar_file=$1
+
+    echo "Uploading JAR file to Flink: $jar_file..."
+    UPLOAD_RESPONSE=$(curl -X POST -H "Expect:" -F "jarfile=@./$jar_file" "http://localhost:8081/jars/upload")
+
+    if [ $? -ne 0 ]; then
+        echo "JAR upload failed. Exiting..."
+        exit 1
+    fi
+
+    echo "JAR uploaded successfully."
+
+    # Extract the uploaded JAR ID
+    JAR_ID=$(curl -s http://localhost:8081/jars | jq -r '.files[0].id')
+
+    if [ -z "$JAR_ID" ]; then
+        echo "Failed to get JAR ID. Exiting..."
+        exit 1
+    fi
+
+    echo "JAR ID: $JAR_ID"
+
+    # Run the JAR file in Flink
+    echo "Starting Flink job for $jar_file..."
+    RUN_RESPONSE=$(curl -X POST "http://localhost:8081/jars/$JAR_ID/run")
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to start Flink job. Exiting..."
+        exit 1
+    fi
+
+    echo "Flink job started successfully for $jar_file."
+}
+
+upload_and_run_jar "project-stream-processor-1.0.0.jar"
+upload_and_run_jar "dashboard-creator-1.0.0.jar"
