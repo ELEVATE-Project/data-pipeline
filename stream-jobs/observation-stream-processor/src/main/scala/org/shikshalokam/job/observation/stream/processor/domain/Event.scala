67e0d51360b06b0420b1e060e4a668f82179ee59
prashanth <prashanth@shikshalokam.org>
2025-05-06 16:30:17 +0530

    added the status,domain and question csv logic

--- stream-jobs/observation-stream-processor/src/main/scala/org/shikshalokam/job/observation/stream/processor/domain/Event.scala
+++ stream-jobs/observation-stream-processor/src/main/scala/org/shikshalokam/job/observation/stream/processor/domain/Event.scala
@@ -8,21 +8,45 @@ class Event(eventMap: java.util.Map[String, Any], partition: Int, offset: Long) 
 
   def solutionId: String = readOrDefault[String]("solutionId", "")
 
+  def solutionName : String = readOrDefault[String]("solutionInfo.name", "")
+
   def createdBy: String = readOrDefault[String]("createdBy", "")
 
   def status: String = readOrDefault[String]("status", "")
 
   def submissionNumber: Integer = readOrDefault[Integer]("submissionNumber", 0)
 
+  def programName: String = readOrDefault[String]("programInfo.name", "")
+
+  def programId: String = readOrDefault[String]("programInfo._id", "")
+
+  def observationName : String = readOrDefault[String]("observationInformation.name", "")
+
+  def observationId : String = readOrDefault[String]("observationId", "")
+
   def stateName: String = readOrDefault[String]("userProfile.state.label", "")
+
+  def stateId: String = readOrDefault[String]("userProfile.state.value", "")
 
   def districtName: String = readOrDefault[String]("userProfile.district.label", "")
 
+  def districtId: String = readOrDefault[String]("userProfile.district.value", "")
+
   def blockName: String = readOrDefault[String]("userProfile.block.label", "")
+
+  def blockId: String = readOrDefault[String]("userProfile.block.value", "")
 
   def clusterName: String = readOrDefault[String]("userProfile.cluster.label", "")
 
+  def clusterId: String = readOrDefault[String]("userProfile.cluster.value", "")
+
+  def orgName: String = readOrDefault[String]("userProfile.organization.name", "")
+
+  def orgId: String = readOrDefault[Int]("userProfile.organization.id", 0).toString
+
   def schoolName: String = readOrDefault[String]("entityInformation.name", "")
+
+  def schoolId : String = readOrDefault[String]("entityId", "")
 
   def pointsBasedMaxScore: Integer = readOrDefault[Integer]("pointsBasedMaxScore", 0)
 
@@ -34,8 +58,11 @@ class Event(eventMap: java.util.Map[String, Any], partition: Int, offset: Long) 
 
   def answers: Map[String, Any] = readOrDefault[Map[String, Any]]("answers", null)
 
+  def userRoles: List[Map[String, Any]] = readOrDefault[List[Map[String, Any]]]("userProfile.user_roles", null)
+
   def isRubric: Boolean = readOrDefault[Boolean]("isRubricDriven", false)
 
-  def solutionName : String = readOrDefault[String]("solutionInfo.name", "")
+  def completedDate: String = readOrDefault[String]("completedDate", "")
+
 }
 
