2731d5380b2f34d3e2a29d89a6144b87e6e5eae9
prashanth <prashanth@shikshalokam.org>
2025-05-12 12:18:43 +0530

    added the observation dashboard creator event

--- metabase-jobs/observation-dashboard-creator/src/main/scala/org/shikshalokam/job/observation/dashboard/creator/domain/Event.scala
+++ metabase-jobs/observation-dashboard-creator/src/main/scala/org/shikshalokam/job/observation/dashboard/creator/domain/Event.scala
@@ -8,16 +8,12 @@ class Event(eventMap: java.util.Map[String, Any], partition: Int, offset: Long) 
 
   def reportType: String = readOrDefault[String]("reportType", "")
 
-  def solution_id: String = readOrDefault("solution_id", "")
-
-  def chartType: List[String] = readOrDefault[List[String]]("chart_type", List.empty[String])
-
-  def solutionName: String = readOrDefault("solutionName", "")
-
   def admin: String = readOrDefault("dashboardData.admin", "")
 
-  def isRubric: String = readOrDefault("isRubric", "")
+  def isRubric: String = readOrDefault("dashboardData.isRubric", "")
 
   def targetedProgram: String = readOrDefault("dashboardData.targetedProgram", "")
+
+  def targetedSolution: String = readOrDefault("dashboardData.targetedSolution", "")
 
 }
