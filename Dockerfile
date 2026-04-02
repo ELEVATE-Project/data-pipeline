30c1a5ebcb0f6908b7b600572be9bdd494946107
Vivek M <125434153+Vivek-M-08@users.noreply.github.com>
2026-04-02 16:24:17 +0530

    elevate data container supervisor added

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,9 @@ RUN apt update && apt install -y \
     tar \
     git \
     curl \
+    cron \
     maven \
+    tmux \
     jq \
     nano \
     postgresql postgresql-client \
@@ -62,7 +64,7 @@ WORKDIR /app
 
 COPY . /app
 
-RUN pip install --no-cache-dir -r /app/Documentation/batch-scripts/requirements.txt
+RUN pip install --no-cache-dir -r /app/requirements.txt
 
 RUN mvn clean install -DskipTests
 
--- akka-service/src/main/scala/org/shikshalokam/job/akkaservice/config/AppConfig.scala
+++ akka-service/src/main/scala/org/shikshalokam/job/akkaservice/config/AppConfig.scala
@@ -4,15 +4,18 @@ import com.typesafe.config.{Config, ConfigFactory}
 import java.io.File
 
 object AppConfig {
-  private val configPath = "unified-test.conf" //Change this to unified-test.conf for testing
-
-  private val rootConfig = if (new File(configPath).exists()) {
-    println(s"[INFO] Loading config from: $configPath")
-    ConfigFactory.parseFile(new File(configPath)).resolve()
-  } else {
-    println(s"[INFO] File not found: $configPath, loading from classpath")
-    ConfigFactory.load(configPath)
+  val config: Config = {
+    val configuredFile = System.getProperty("config.file", "unified-common.conf")
+    val file = new File(configuredFile)
+    
+    val rootConfig = if (file.exists()) {
+      println(s"[INFO] Loading config from resolved path: ${file.getAbsolutePath}")
+      ConfigFactory.parseFile(file).resolve()
+    } else {
+      println(s"[INFO] Config file not found on disk, attempting to load from classpath: $configuredFile")
+      ConfigFactory.load(configuredFile)
+    }
+    
+    rootConfig.withFallback(ConfigFactory.load())
   }
-
-  val config: Config = rootConfig.withFallback(ConfigFactory.load())
 }
